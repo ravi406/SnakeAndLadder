76767801123edb1caec9af65cbb10a7fa7dd776c
ravi406 <ravikamble406@gmail.com>
2022-11-25 12:51:45 +0530

    Dice roll method added to rolldice to generate random number from 1 to 6

--- src/com/snakeladder/SnakeAndLadder.java
+++ src/com/snakeladder/SnakeAndLadder.java
@@ -1,13 +1,24 @@
 package com.snakeladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake And Ladder Program");
-		
+
 		int position = 0;
-		
-		System.out.println("Single Player Starting Position :"+position);
+
+		System.out.println("Single Player Starting Position :" + position);
+
+		rolldice();
 	}
 
+	private static void rolldice() {			/* here im defining rolldice method to take random number from 1 to 6 */
+		Random random = new Random();
+		int dice = random.nextInt(6);
+		dice = dice + 1;
+		System.out.println("Dice Number :" + dice);
+
+	}
 }
