b05776f31046325f1f04f4545e9324783281af9b
ravi406 <ravikamble406@gmail.com>
2022-11-25 12:06:27 +0530

    added starting user position in  Program

--- src/com/snakeladder/SnakeAndLadder.java
+++ src/com/snakeladder/SnakeAndLadder.java
@@ -4,6 +4,10 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake And Ladder Program");
+		
+		int position = 0;
+		
+		System.out.println("Single Player Starting Position :"+position);
 	}
 
 }
